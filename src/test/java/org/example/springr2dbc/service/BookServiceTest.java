ac22919385f51c9e43e723330cf774b4d6a5f52a
Md. Giash Uddin <giash.inument@bkash.com>
2024-05-11 14:03:44 +0600

    Added unit test for service

--- src/test/java/org/example/springr2dbc/service/BookServiceTest.java
+++ src/test/java/org/example/springr2dbc/service/BookServiceTest.java
@@ -10,13 +10,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.security.SecureRandom;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,17 +41,34 @@ public class BookServiceTest {
 
     @ParameterizedTest
     @MethodSource("getAllBooks")
-    void testGetAllBooks(Flux<Book> books) {
-        when(bookRepository.findAll()).thenReturn(books);
-        Flux<BookResponse> allBooks = bookService.getAllBooks();
+    void testGetAllBooks(Flux<Book> bookFlux, List<Book> books) {
+        when(bookRepository.findAll()).thenReturn(bookFlux);
+        Flux<BookResponse> bookResponseFlux = bookService.getAllBooks();
+        Mono<List<BookResponse>> listMono = bookResponseFlux.collectList();
+
+        listMono.subscribe(
+                result -> {
+                    assertEquals(result.size(), books.size());
+                    assertEquals(result.getFirst().getId(), books.getFirst().getId());
+                    assertEquals(result.getFirst().getAvailable(), books.getFirst().getAvailable());
+                    assertEquals(result.getFirst().getName(), books.getFirst().getName());
+                    assertEquals(result.getFirst().getPrice(), books.getFirst().getPrice());
+                    assertEquals(result.getFirst().getPublishedDate(), books.getFirst().getPublishedDate());
+                    assertEquals(result.getFirst().getAuthorName(), books.getFirst().getAuthorName());
+                }
+        );
     }
 
 
     private Stream<Arguments> getAllBooks() {
+        List<Book> bookList1 = Arrays.asList(createBookObject(), createBookObject());
+        List<Book> bookList2 = Arrays.asList(createBookObject(), createBookObject());
+        List<Book> bookList3 = Arrays.asList(createBookObject(), createBookObject());
+
         return Stream.of(
-                Arguments.of(Flux.just(createBookObject(), createBookObject(), createBookObject())),
-                Arguments.of(Flux.just(createBookObject(), createBookObject(), createBookObject())),
-                Arguments.of(Flux.just(createBookObject(), createBookObject(), createBookObject()))
+                Arguments.of(Flux.just(bookList1.getFirst(), bookList1.get(1)), bookList1),
+                Arguments.of(Flux.just(bookList2.getFirst(), bookList2.get(1)), bookList2),
+                Arguments.of(Flux.just(bookList3.getFirst(), bookList3.get(1)), bookList3)
         );
     }
 
